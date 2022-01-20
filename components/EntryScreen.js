692782beed6b169eac254a6b56e5e7dc692a2c3e
zrodr22 <zrodr24@gmail.com>
2022-01-20 02:53:59 -0800

    Page component added dynamically. Focus page next

--- components/EntryScreen.js
+++ components/EntryScreen.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import { View, TextInput, Button} from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { View, TextInput, Button, Text, ScrollView} from 'react-native';
 import { StyleSheet, ImageBackground } from 'react-native';
+import { nativeViewGestureHandlerProps } from 'react-native-gesture-handler/lib/typescript/handlers/NativeViewGestureHandler';
+import { useAnimatedGestureHandler } from 'react-native-reanimated';
 
 const EntryInput = (props) => {
   return (
@@ -11,11 +13,77 @@ const EntryInput = (props) => {
   );
 }
 
+const Page = (props) => {
+  return(
+    <View style={{flexDirection:'row',justifyContent : 'space-between', height: 750, width: '50%'}}>
+      <View style={{backgroundColor: props.paperColor, height: 715, width: 20, borderWidth: 1, borderColor: 'gray'}}></View>
+      <View style={{backgroundColor: props.paperColor, height: 715, width: 500, borderWidth: 1, borderColor: 'gray'}}>
+          <EntryInput
+              {...props}
+              onContentSizeChange={event => 
+                {
+                  const h = event.nativeEvent.contentSize.height;
+                  if (h > 660) {
+                        const updatedValues = [...props.values];
+                        updatedValues.push('')
+                        props.updateValues(updatedValues)
+
+                        const currentPages = props.pages;
+                        const updatedPages = [...props.pages];
+                        updatedPages.push(currentPages[currentPages.length - 1] + 1)
+                        props.addPage(updatedPages);
+                  }
+                }
+              }
+              placeholder="Entry:"
+              multiline={true}
+              style={{padding: 10, backgroundColor: props.paperColor}}
+          />
+      </View>
+    </View>
+  )
+}
+
+const updateText = (newText, oldText) => {
+  const updatedText = oldText + newText;
+  return updatedText; 
+}
+
+const calcNumLines = (height) => {
+  // return (height-42.4)/18.4 + 1;
+  return height;
+}
+
 const EntryScreen = (props) => {
     const paperColor = 'black'
     const date = 'December 19, 2021\n\nEntry: ';
-    const [value, onChangeText] = React.useState(date);
+    const [values, updateValues] = useState([date]);
+    const [pages, addPage] = useState([0]);
     const link = 'https://c.tenor.com/RVRuH78_4H0AAAAd/snow-snowing.gif'
+    const w = 749;
+
+    const [displayText, setDisplay] = useState("waiting");
+
+    // Need to focus the next page after adding new page
+    let NewPage = pages.map((page, index) => {
+      return(
+        <Page 
+          key={index} 
+          onChangeText={text => {
+            const updatedValues = [...values];
+            updatedValues[index] = text;
+            updateValues(updatedValues);
+            }
+          }
+          paperColor={paperColor}
+          value={values[index]} 
+          values={values}
+          addPage={addPage}
+          updateValues={updateValues}
+          pages={pages}
+        />
+      )
+    })
 
     return (
         <ImageBackground source={{uri: link}} style={{flex: 1, width:'100%', height: '100%', alignItems: 'center', justifyContent: 'center' }} >
@@ -25,15 +93,22 @@ const EntryScreen = (props) => {
             title='Back to Home'
             onPress={() => props.navigation.push("Home")}
             />
-            <View style={{backgroundColor: paperColor, height: '95%', width: '50%', borderWidth: 1, borderColor: 'gray'}}>
-                <EntryInput
-                    onChangeText={text => onChangeText(text)}
-                    value={value}
-                    placeholder="Entry:"
-                    multiline={true}
-                    style={{padding: 10, backgroundColor: paperColor}}
-                />
-            </View>
+            <Button
+            title='Send a request'
+            onPress={async () => {
+              const res = await fetch('http://localhost:8000/polls/')
+              const body = res._bodyText
+              setDisplay(body)
+              console.log(body)
+              }}
+            />
+            <Text>
+              {displayText}
+            </Text>
+
+            <ScrollView style={{flexDirection:'column', width: 500}}>
+              {NewPage}
+            </ScrollView>
         </ImageBackground>
     )
 }
